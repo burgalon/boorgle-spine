2c57bfbb53d5bd9ef504d250286476b726fd3d69
Alon Burg <burgalon@gmail.com>
2012-08-21 00:49:33 -0400

    helper script for building and running the trigger.io app

---
+++ run-ios.bash
@@ -0,0 +1,1 @@
+hem build && cp public/application.css src/application.css && cp public/application.js src/application.js && $HOME/Library/Trigger\ Toolkit/forge build && $HOME/Library/Trigger\ Toolkit/forge run ios
