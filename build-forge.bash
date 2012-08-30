8c74f82ee0f185e98538a62d5e88feb66854a6d5
Alon Burg <burgalon@gmail.com>
2012-08-30 00:34:25 -0400

    build-forge.bash update

--- build-forge.bash
+++ build-forge.bash
@@ -2,6 +2,7 @@
 hem build
 cp public/application.css src/application.css 
 cp public/application.js src/application.js
+cp public/index.html src/index.html
 cp -r public/images src/
 forge build
 rm public/application.css public/application.js
