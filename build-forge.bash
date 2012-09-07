b01186de4707f00f48ff27047c83f03c8c20000d
Alon Burg <burgalon@gmail.com>
2012-09-07 00:19:48 -0400

    build-forge script

--- build-forge.bash
+++ build-forge.bash
@@ -1,9 +1,8 @@
 #!/bin/sh
 hem build
-cp public/application.css src/application.css 
-cp public/application.js src/application.js
+mv public/application.css src/application.css
+mv public/application.js src/application.js
 cp public/index.html src/index.html
 cp -r public/images src/
 forge build
-rm public/application.css public/application.js
 #forge run ios
