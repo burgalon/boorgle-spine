f4780dbdc07e4c90b69bc9bd32df21debc55e281
Alon Burg <burgalon@gmail.com>
2012-10-29 22:38:19 -0400

    fix run script

--- run-ios.bash
+++ run-ios.bash
@@ -1,2 +1,2 @@
 #!/bin/sh
-hem build && cp public/application.css src/application.css && cp public/application.js src/application.js && $HOME/Library/Trigger\ Toolkit/forge build && $HOME/Library/Trigger\ Toolkit/forge run ios
+hem build && cp public/application.css src/application.css && cp public/application.js src/application.js && $HOME/Library/Trigger\ Toolkit/forge build $1 && $HOME/Library/Trigger\ Toolkit/forge run $1
