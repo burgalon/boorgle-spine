c694198e8b9a1e74bbd15bb13b93725f5a191f9d
Alon Burg <burgalon@gmail.com>
2012-10-29 22:30:49 -0400

    add shebang to bash file

--- run-ios.bash
+++ run-ios.bash
@@ -1,1 +1,2 @@
+#!/bin/sh
 hem build && cp public/application.css src/application.css && cp public/application.js src/application.js && $HOME/Library/Trigger\ Toolkit/forge build && $HOME/Library/Trigger\ Toolkit/forge run ios
