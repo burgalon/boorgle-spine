18e8035eba85d172e04878c8f0f7defc620b3881
Alon Burg <burgalon@gmail.com>
2012-11-04 20:36:55 -0500

    run-ios to delete hem build generated files

--- run-ios.bash
+++ run-ios.bash
@@ -1,3 +1,3 @@
 #!/bin/sh
 env=${1:-ios}
-hem build && cp public/application.css src/application.css && cp public/application.js src/application.js && $HOME/Library/Trigger\ Toolkit/forge build $env && $HOME/Library/Trigger\ Toolkit/forge run $env
+hem build && cp public/application.css src/application.css && cp public/application.js src/application.js && $HOME/Library/Trigger\ Toolkit/forge build $env && rm public/application.css public/application.js && $HOME/Library/Trigger\ Toolkit/forge run $env
